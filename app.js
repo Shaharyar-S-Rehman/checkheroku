a18a5ff30cf4612b5d887f63180c2107a968e547
Shaharyar-S-Rehman <shaharyarrehman79@gmail.com>
2021-10-11 22:59:54 +0500

    any name for commit

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const PORT = process.env.PORT || 3000
 const app = express()
+const path = require('path')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+
+app.use('/', express.static(path.join(__dirname, 'web/build')))
+
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
 
 
 app.get('/profile', (req, res) => {
---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import Todo from "./component/todo/todo";
+
+function App() {
+
+
+  return (
+    <>
+      <div className="m-auto" >
+        <Todo />
+      </div>
+    </>
+  )
+}
+export default App;
